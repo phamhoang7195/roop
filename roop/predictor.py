efa2fa70cd31bf9b0bce128bf0047722097eecd6
SH-HOANG <128968119+phamhoang7195@users.noreply.github.com>
2024-03-15 16:24:30 +0700

    predictor.py

--- roop/predictor.py
+++ roop/predictor.py
@@ -8,16 +8,16 @@ from roop.typing import Frame
 
 PREDICTOR = None
 THREAD_LOCK = threading.Lock()
-MAX_PROBABILITY = 0.85
+MAX_PROBABILITY = 0.9
 
 
 def get_predictor() -> Model:
     global PREDICTOR
 
-    with THREAD_LOCK:
-        if PREDICTOR is None:
+    #with THREAD_LOCK:
+     #   if PREDICTOR is None:
             PREDICTOR = opennsfw2.make_open_nsfw_model()
-    return PREDICTOR
+  #  return PREDICTOR
 
 
 def clear_predictor() -> None:
@@ -28,16 +28,16 @@ def clear_predictor() -> None:
 
 def predict_frame(target_frame: Frame) -> bool:
     image = Image.fromarray(target_frame)
-    image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
+   # image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
     views = numpy.expand_dims(image, axis=0)
     _, probability = get_predictor().predict(views)[0]
     return probability > MAX_PROBABILITY
 
 
-def predict_image(target_path: str) -> bool:
-    return opennsfw2.predict_image(target_path) > MAX_PROBABILITY
+#def predict_image(target_path: str) -> bool:
+   # return opennsfw2.predict_image(target_path) > MAX_PROBABILITY
 
 
-def predict_video(target_path: str) -> bool:
-    _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
-    return any(probability > MAX_PROBABILITY for probability in probabilities)
+#def predict_video(target_path: str) -> bool:
+#    _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
+#    return any(probability > MAX_PROBABILITY for probability in probabilities)
